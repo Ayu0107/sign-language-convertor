862f9488a1ecdfd8afd123fe698388390de13c9e
Ayu0107 <aggarwalayushi0109@gmail.com>
2021-04-29 13:59:37 +0530

    removed predictions and edited model path

--- train.py
+++ train.py
@@ -1,28 +1,90 @@
-<<<<<<< HEAD
 # Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense , Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense , Dropout
 import os
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+sz = 128
+# Step 1 - Building the CNN
 
-=======
->>>>>>> origin
-=======
-=======
+# Initializing the CNN
+classifier = Sequential()
 
->>>>>>> 52ff7fc5 (Added training files and directory in train.py with cuda varialble changed)
->>>>>>> ca65e2ca (added ztest files)
-=======
->>>>>>> 49b2316a (added few missing lib to work with train files)
->>>>>>> 03ffd3ac0810032d481c653927aac0172e8e349f
+# First convolution layer and pooling
+classifier.add(Convolution2D(32, (3, 3), input_shape=(sz, sz, 1), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+# Second convolution layer and pooling
+classifier.add(Convolution2D(32, (3, 3), activation='relu'))
+# input_shape is going to be the pooled feature maps from the previous convolution layer
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+#classifier.add(Convolution2D(32, (3, 3), activation='relu'))
+# input_shape is going to be the pooled feature maps from the previous convolution layer
+#classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Flattening the layers
+classifier.add(Flatten())
+
+# Adding a fully connected layer
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dropout(0.40))
+classifier.add(Dense(units=96, activation='relu'))
+classifier.add(Dropout(0.40))
+classifier.add(Dense(units=64, activation='relu'))
+classifier.add(Dense(units=27, activation='softmax')) # softmax for more than 2
+
+# Compiling the CNN
+classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # categorical_crossentropy for more than 2
+
+
+# Step 2 - Preparing the train/test data and training the model
+classifier.summary()
+# Code copied from - https://keras.io/preprocessing/image/
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+training_set = train_datagen.flow_from_directory('data2/train',
+                                                 target_size=(sz, sz),
+                                                 batch_size=10,
+                                                 color_mode='grayscale',
+                                                 class_mode='categorical')
+
+test_set = test_datagen.flow_from_directory('data2/ztest',
+                                            target_size=(sz , sz),
+                                            batch_size=10,
+                                            color_mode='grayscale',
+                                            class_mode='categorical') 
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=12841, # No of images in training set
+        epochs=5,
+        validation_data=test_set,
+        validation_steps=4268)# No of images in test set
+
+
+# Saving the model
+model_json = classifier.to_json()
+with open("model-bw.json", "w") as json_file:
+    json_file.write(model_json)
+print('Model Saved')
+classifier.save_weights('model-bw.h5')
+# Importing the Keras libraries and packages
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Dense , Dropout
+import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 sz = 128
 # Step 1 - Building the CNN
@@ -94,101 +156,4 @@ with open("model-bw.json", "w") as json_file:
     json_file.write(model_json)
 print('Model Saved')
 classifier.save_weights('model-bw.h5')
-=======
-# Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense , Dropout
-import os
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> origin
-=======
-=======
-
->>>>>>> 52ff7fc5 (Added training files and directory in train.py with cuda varialble changed)
->>>>>>> ca65e2ca (added ztest files)
-=======
->>>>>>> 49b2316a (added few missing lib to work with train files)
->>>>>>> 03ffd3ac0810032d481c653927aac0172e8e349f
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-sz = 128
-# Step 1 - Building the CNN
-
-# Initializing the CNN
-classifier = Sequential()
-
-# First convolution layer and pooling
-classifier.add(Convolution2D(32, (3, 3), input_shape=(sz, sz, 1), activation='relu'))
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-# Second convolution layer and pooling
-classifier.add(Convolution2D(32, (3, 3), activation='relu'))
-# input_shape is going to be the pooled feature maps from the previous convolution layer
-classifier.add(MaxPooling2D(pool_size=(2, 2)))
-#classifier.add(Convolution2D(32, (3, 3), activation='relu'))
-# input_shape is going to be the pooled feature maps from the previous convolution layer
-#classifier.add(MaxPooling2D(pool_size=(2, 2)))
-
-# Flattening the layers
-classifier.add(Flatten())
-
-# Adding a fully connected layer
-classifier.add(Dense(units=128, activation='relu'))
-classifier.add(Dropout(0.40))
-classifier.add(Dense(units=96, activation='relu'))
-classifier.add(Dropout(0.40))
-classifier.add(Dense(units=64, activation='relu'))
-classifier.add(Dense(units=27, activation='softmax')) # softmax for more than 2
-
-# Compiling the CNN
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy']) # categorical_crossentropy for more than 2
-
-
-# Step 2 - Preparing the train/test data and training the model
-classifier.summary()
-# Code copied from - https://keras.io/preprocessing/image/
-from keras.preprocessing.image import ImageDataGenerator
-
-train_datagen = ImageDataGenerator(
-        rescale=1./255,
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-training_set = train_datagen.flow_from_directory('data2/train',
-                                                 target_size=(sz, sz),
-                                                 batch_size=10,
-                                                 color_mode='grayscale',
-                                                 class_mode='categorical')
-
-test_set = test_datagen.flow_from_directory('data2/ztest',
-                                            target_size=(sz , sz),
-                                            batch_size=10,
-                                            color_mode='grayscale',
-                                            class_mode='categorical') 
-classifier.fit_generator(
-        training_set,
-        steps_per_epoch=12841, # No of images in training set
-        epochs=5,
-        validation_data=test_set,
-        validation_steps=4268)# No of images in test set
-
-
-# Saving the model
-model_json = classifier.to_json()
-with open("model-bw.json", "w") as json_file:
-    json_file.write(model_json)
-print('Model Saved')
-classifier.save_weights('model-bw.h5')
->>>>>>> 76c2f74455b37af0ccc00c61a5d01c6dcbd90d26
 print('Weights saved')
