43f906b7e1a826667e09e2802a9101d829faa6d4
mahi101101 <mpavanteja2001@gmail.com>
2021-04-29 17:36:17 +0530

    train json data updated

--- train.py
+++ train.py
@@ -120,12 +120,8 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 training_set = train_datagen.flow_from_directory('data2/train',target_size=(sz, sz),batch_size=1,color_mode='grayscale',class_mode='categorical')
 
 test_set = test_datagen.flow_from_directory('data2/ztest',target_size=(sz , sz),batch_size=1,color_mode='grayscale',class_mode='categorical') 
-classifier.fit_generator(
-        training_set,
-        steps_per_epoch=12845, # No of images in training set
-        epochs=5,
-        validation_data=test_set,
-        validation_steps=4268)# No of images in test set
+classifier.fit_generator(training_set , steps_per_epoch = 12845, epochs=5 , validation_data = test_set  , validation_steps = 4268 )
+# No of images in test set
 
 
 # Saving the model
